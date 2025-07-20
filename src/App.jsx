a2d5be085120368e05232e81cab182d7f0c367ae
Surya-Konakanchi <mailsuryak.java@gmail.com>
2025-07-20 00:48:52 -0400

    Adding Code Owners

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="app-container">HELLOO</h1>;
+  return <h1 className="app-container">Hello, Welcome to Vehical Rental Application UI App</h1>;
 }
 
 export default App;
